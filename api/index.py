c2b9d5b865a01cf5c9860ba14d76f64d33fac0ca
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-07-27 16:05:55 +0700

    lab objective 2, test on local success, continue test in vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/api/python")
+
+@app.route("/api-proxy/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/:path*',
+        source: '/api-proxy/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+            ? 'http://127.0.0.1:5328/api-proxy/:path*'
+            : '/api-proxy/',
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
